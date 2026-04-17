50d6672f654ad37c47a21e79cb62ad5e673cd68d
Bruh <vasickinfedor@gmail.com>
2026-04-17 10:25:07 +0300

    first commit

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,24 @@
+import './Head.css';
+
+function head() {
+  return (
+    <div className="App">
+        
+
+<div className="head-line">
+  <div className="header__logo">
+    <img src="../logo-ProSkills.png" alt="логотип" />
+  </div>
+  <ul className="header__nav">
+    <li><a href="">Проекты студентов</a></li>
+    <li><a href="">Отзывы</a></li>
+    <li><a href="">О нас</a></li>
+  </ul>
+</div>
+        
+        
+    </div>
+  );
+}
+
+export default head;
